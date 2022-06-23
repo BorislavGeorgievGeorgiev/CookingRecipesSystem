e9a90c4837dcd6369c8835c731c6bb6ad73c4835
Borislav <Borislav.G.Georgiev@gmail.com>
2022-06-23 13:55:42 +0300

    Extract EntityExtensions.

---
+++ Server/tests/Domain.UnitTests/Common/EntityExtensions.cs
@@ -0,0 +1,31 @@
+﻿using CookingRecipesSystem.Domain.Common;
+
+namespace CookingRecipesSystem.Domain.UnitTests.Common
+{
+	internal static class EntityExtensions
+	{
+		/// <summary>
+		/// Extend the current class to use Entity&lt;int&gt; extension method.
+		/// </summary>
+		/// <typeparam name="TEntity"></typeparam>
+		/// <param name="entity"></param>
+		/// <param name="id"></param>
+		/// <returns>Extended current class.</returns>
+		internal static TEntity SetId<TEntity>(this TEntity entity, int id)
+				where TEntity : Entity<int>
+				=> (entity.SetId<int>(id) as TEntity)!;
+
+		private static Entity<T> SetId<T>(this Entity<T> entity, int id)
+				where T : struct
+		{
+			entity
+					.GetType()
+					.BaseType!
+					.GetProperty(nameof(Entity<T>.Id))!
+					.GetSetMethod(true)!
+					.Invoke(entity, new object[] { id });
+
+			return entity;
+		}
+	}
+}
--- Server/tests/Domain.UnitTests/Common/EntityTests.cs
+++ Server/tests/Domain.UnitTests/Common/EntityTests.cs
@@ -6,6 +6,8 @@ namespace CookingRecipesSystem.Domain.UnitTests.Common
 {
 	public class EntityTests
 	{
+		private class TestEntity : Entity<int> { }
+
 		[Fact]
 		public void Entities_With_Equal_Ids_Should_Be_Equal()
 		{
@@ -32,28 +34,6 @@ namespace CookingRecipesSystem.Domain.UnitTests.Common
 
 			// Assert
 			result.Should().BeFalse();
-		}
-
-		private class TestEntity : Entity<int> { }
-	}
-
-	internal static class EntityExtensions
-	{
-		public static TEntity SetId<TEntity>(this TEntity entity, int id)
-				where TEntity : Entity<int>
-				=> (entity.SetId<int>(id) as TEntity)!;
-
-		private static Entity<T> SetId<T>(this Entity<T> entity, int id)
-				where T : struct
-		{
-			entity
-					.GetType()
-					.BaseType!
-					.GetProperty(nameof(Entity<T>.Id))!
-					.GetSetMethod(true)!
-					.Invoke(entity, new object[] { id });
-
-			return entity;
 		}
 	}
 }
