85f4b42c218824c257c3e76935a970ae0e128d63
Borislav <Borislav.G.Georgiev@gmail.com>
2022-06-24 19:52:43 +0300

    Add tests for UserLoginRequestModelTests.cs and UserTokenResponseModelTests.cs

---
+++ Server/tests/Application.UnitTests/Common/Models/UserLoginRequestModelTests.cs
@@ -0,0 +1,53 @@
+﻿
+using CookingRecipesSystem.Application.Common.Models;
+
+namespace CookingRecipesSystem.Application.UnitTests.Common.Models
+{
+	public class UserLoginRequestModelTests
+	{
+		private const string EmailString = "test@test.com";
+		private const string PasswordString = "strongPassword";
+
+		[Fact]
+		public void Public_String_Email_Property_Should_Exist()
+		{
+			// Arrange			
+			const string EmailPropertyName = "Email";
+			var loginModel = new UserLoginRequestModel(EmailString, PasswordString);
+
+			// Act & Assert
+			Assert.True(loginModel.GetType().GetProperty(EmailPropertyName, typeof(string)) != null);
+		}
+
+		[Fact]
+		public void Public_String_Password_Property_Should_Exist()
+		{
+			// Arrange			
+			const string PasswordPropertyName = "Password";
+			var loginModel = new UserLoginRequestModel(EmailString, PasswordString);
+
+			// Act & Assert
+			Assert.True(loginModel.GetType().GetProperty(PasswordPropertyName, typeof(string)) != null);
+		}
+
+		[Fact]
+		public void Email_Should_Be_Set_Correctly()
+		{
+			// Arrange
+			var loginModel = new UserLoginRequestModel(EmailString, PasswordString);
+
+			// Act & Assert
+			Assert.True(loginModel.Email == EmailString);
+		}
+
+		[Fact]
+		public void Password_Should_Be_Set_Correctly()
+		{
+			// Arrange
+			var loginModel = new UserLoginRequestModel(EmailString, PasswordString);
+
+			// Act & Assert
+			Assert.True(loginModel.Password == PasswordString);
+		}
+	}
+}
---
+++ Server/tests/Application.UnitTests/Common/Models/UserTokenResponseModelTests.cs
@@ -0,0 +1,29 @@
+﻿using CookingRecipesSystem.Application.Common.Models;
+
+namespace CookingRecipesSystem.Application.UnitTests.Common.Models
+{
+	public class UserTokenResponseModelTests
+	{
+		[Fact]
+		public void Public_String_Token_Property_Should_Exist()
+		{
+			// Arrange
+			const string TokenPropertyName = "Token";
+			var tokenModel = new UserTokenResponseModel("someString");
+
+			// Act & Assert
+			Assert.True(tokenModel.GetType().GetProperty(TokenPropertyName, typeof(string)) != null);
+		}
+
+		[Fact]
+		public void Token_Should_Be_Set_Correctly()
+		{
+			// Arrange
+			var stringToken = "token";
+			var tokenModel = new UserTokenResponseModel(stringToken);
+
+			// Act & Assert
+			Assert.True(tokenModel.Token == stringToken);
+		}
+	}
+}
