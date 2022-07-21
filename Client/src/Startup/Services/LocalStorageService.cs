59b8b05a404888caf4501e1ea80f66a0b9790157
Borislav <Borislav.G.Georgiev@gmail.com>
2022-07-21 19:42:37 +0300

    Start adding Authentication to Client.

---
+++ Client/src/Startup/Services/LocalStorageService.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+using Microsoft.JSInterop;
+
+namespace CookingRecipesSystem.Startup.Services
+{
+	public interface ILocalStorageService
+	{
+		Task<T> GetItem<T>(string key);
+		Task SetItem<T>(string key, T value);
+		Task RemoveItem(string key);
+	}
+
+	public class LocalStorageService : ILocalStorageService
+	{
+		private IJSRuntime _jsRuntime;
+
+		public LocalStorageService(IJSRuntime jsRuntime)
+		{
+			this._jsRuntime = jsRuntime;
+		}
+
+		public async Task<T> GetItem<T>(string key)
+		{
+			var json = await this._jsRuntime.InvokeAsync<string>("localStorage.getItem", key);
+
+			if (json == null)
+			{
+				return default;
+			}
+
+			return JsonSerializer.Deserialize<T>(json);
+		}
+
+		public async Task SetItem<T>(string key, T value)
+		{
+			await this._jsRuntime.InvokeVoidAsync("localStorage.setItem", key, JsonSerializer.Serialize(value));
+		}
+
+		public async Task RemoveItem(string key)
+		{
+			await this._jsRuntime.InvokeVoidAsync("localStorage.removeItem", key);
+		}
+	}
+}
