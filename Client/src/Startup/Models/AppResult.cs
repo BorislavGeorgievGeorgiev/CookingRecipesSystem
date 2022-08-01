49f19f01dd7bfd584e8b16a24ebeac91d3d2eb98
Borislav <Borislav.G.Georgiev@gmail.com>
2022-08-01 19:17:02 +0300

    Add try-catch for GetAll().

--- Client/src/Startup/Models/AppResult.cs
+++ Client/src/Startup/Models/AppResult.cs
@@ -1,20 +1,20 @@
 ﻿namespace CookingRecipesSystem.Startup.Models
 {
-	public class AppResult
-	{
-		public bool Succeeded { get; set; }
+  public class AppResult
+  {
+    public bool Succeeded { get; set; }
 
-		public IEnumerable<string> Errors { get; set; }
+    public IEnumerable<string> Errors { get; set; }
 
-		public EmptyModel Response { get; set; } = default!;
-	}
+    public EmptyModel Response { get; set; } = default!;
+  }
 
-	public class AppResult<TResponse> where TResponse : class
-	{
-		public bool Succeeded { get; set; }
+  public class AppResult<TResponse> where TResponse : class
+  {
+    public bool Succeeded { get; set; }
 
-		public IEnumerable<string> Errors { get; set; }
+    public IEnumerable<string> Errors { get; set; }
 
-		public TResponse Response { get; set; } = default!;
-	}
+    public TResponse Response { get; set; } = default!;
+  }
 }
--- Client/src/Startup/Services/AuthService.cs
+++ Client/src/Startup/Services/AuthService.cs
@@ -8,108 +8,122 @@ using CookingRecipesSystem.Startup.Models;
 
 namespace CookingRecipesSystem.Startup.Services
 {
-	public interface IAuthService
-	{
-		Task<AppResult<LoginResult>> Login(UserLoginModel loginModel);
-		Task Logout();
-		Task<AppResult> Register(UserRegisterModel registerModel);
+  public interface IAuthService
+  {
+    Task<AppResult<LoginResult>> Login(UserLoginModel loginModel);
+    Task Logout();
+    Task<AppResult> Register(UserRegisterModel registerModel);
 
-		Task<AppResult<UsersListModel>> GetAll();
-	}
+    Task<AppResult<UsersListModel>> GetAll();
+  }
 
-	public class AuthService : IAuthService
-	{
-		private const string IdentityPath = "api/Identity/";
-		private const string AuthTokenName = "authToken";
+  public class AuthService : IAuthService
+  {
+    private const string IdentityPath = "api/Identity/";
+    private const string AuthTokenName = "authToken";
 
-		private readonly string? _apiIdentityUri;
-		private readonly HttpClient _httpClient;
-		private readonly IConfiguration _configuration;
-		private readonly ApiAuthenticationStateProvider _authenticationStateProvider;
-		private readonly ILocalStorageService _localStorage;
+    private readonly string? _apiIdentityUri;
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+    private readonly ApiAuthenticationStateProvider _authenticationStateProvider;
+    private readonly ILocalStorageService _localStorage;
 
-		public AuthService(
-			HttpClient httpClient,
-			IConfiguration configuration,
-			ApiAuthenticationStateProvider authenticationStateProvider,
-			ILocalStorageService localStorage)
-		{
-			this._httpClient = httpClient;
-			this._configuration = configuration;
-			this._apiIdentityUri = this._configuration
-				.GetSection(nameof(ApiConfig))
-				.GetSection(nameof(ApiConfig.ApiUrl)).Value + IdentityPath;
-			this._authenticationStateProvider = authenticationStateProvider;
-			this._localStorage = localStorage;
-		}
+    public AuthService(
+      HttpClient httpClient,
+      IConfiguration configuration,
+      ApiAuthenticationStateProvider authenticationStateProvider,
+      ILocalStorageService localStorage)
+    {
+      this._httpClient = httpClient;
+      this._configuration = configuration;
+      this._apiIdentityUri = this._configuration
+        .GetSection(nameof(ApiConfig))
+        .GetSection(nameof(ApiConfig.ApiUrl)).Value + IdentityPath;
+      this._authenticationStateProvider = authenticationStateProvider;
+      this._localStorage = localStorage;
+    }
 
-		public async Task<AppResult> Register(UserRegisterModel registerModel)
-		{
-			var response = await this._httpClient
-				.PostAsJsonAsync(this.GetRequestUri(nameof(Register)), registerModel);
+    public async Task<AppResult> Register(UserRegisterModel registerModel)
+    {
+      var response = await this._httpClient
+        .PostAsJsonAsync(this.GetRequestUri(nameof(Register)), registerModel);
 
-			var registerResult = JsonSerializer
-				.Deserialize<AppResult>(await response.Content.ReadAsStringAsync(),
-				new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+      var registerResult = JsonSerializer
+        .Deserialize<AppResult>(await response.Content.ReadAsStringAsync(),
+        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-			return registerResult!;
-		}
+      return registerResult!;
+    }
 
-		public async Task<AppResult<LoginResult>> Login(UserLoginModel loginModel)
-		{
-			var response = await this._httpClient
-				.PostAsJsonAsync(this.GetRequestUri(nameof(Login)), loginModel);
+    public async Task<AppResult<LoginResult>> Login(UserLoginModel loginModel)
+    {
+      var response = await this._httpClient
+        .PostAsJsonAsync(this.GetRequestUri(nameof(Login)), loginModel);
 
-			var content = await response.Content.ReadAsStringAsync();
+      var content = await response.Content.ReadAsStringAsync();
 
-			var loginResult = JsonSerializer
-				.Deserialize<AppResult<LoginResult>>(await response.Content.ReadAsStringAsync(),
-				new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+      var loginResult = JsonSerializer
+        .Deserialize<AppResult<LoginResult>>(await response.Content.ReadAsStringAsync(),
+        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-			if (!response.IsSuccessStatusCode)
-			{
-				return loginResult!;
-			}
+      if (!response.IsSuccessStatusCode)
+      {
+        return loginResult!;
+      }
 
-			await this._localStorage.SetItemAsync(AuthTokenName, loginResult!.Response.Token);
+      await this._localStorage.SetItemAsync(AuthTokenName, loginResult!.Response.Token);
 
-			var authenticationState = await this._authenticationStateProvider
-				.GetAuthenticationStateAsync();
+      var authenticationState = await this._authenticationStateProvider
+        .GetAuthenticationStateAsync();
 
-			var keyValuePair = authenticationState.User.Claims
-				.FirstOrDefault(c => c.Type == "unique_name");
+      var keyValuePair = authenticationState.User.Claims
+        .FirstOrDefault(c => c.Type == "unique_name");
 
-			var userName = keyValuePair!.Value;
+      var userName = keyValuePair!.Value;
 
-			this._authenticationStateProvider.MarkUserAsAuthenticated(userName);
+      this._authenticationStateProvider.MarkUserAsAuthenticated(userName);
 
-			this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-				"bearer", loginResult.Response.Token);
+      this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+        "bearer", loginResult.Response.Token);
 
-			return loginResult;
-		}
+      return loginResult;
+    }
 
-		public async Task Logout()
-		{
-			await this._localStorage.RemoveItemAsync(AuthTokenName);
+    public async Task Logout()
+    {
+      await this._localStorage.RemoveItemAsync(AuthTokenName);
 
-			this._authenticationStateProvider.MarkUserAsLoggedOut();
+      this._authenticationStateProvider.MarkUserAsLoggedOut();
 
-			this._httpClient.DefaultRequestHeaders.Authorization = null;
-		}
+      this._httpClient.DefaultRequestHeaders.Authorization = null;
+    }
 
-		public async Task<AppResult<UsersListModel>> GetAll()
-		{
-			var uri = this.GetRequestUri(nameof(GetAll));
-			var response = await this._httpClient.GetFromJsonAsync<AppResult<UsersListModel>>(uri);
+    public async Task<AppResult<UsersListModel>> GetAll()
+    {
+      var uri = this.GetRequestUri(nameof(GetAll));
+      AppResult<UsersListModel>? response;
 
-			return response!;
-		}
+      try
+      {
+        response = await this._httpClient.GetFromJsonAsync<AppResult<UsersListModel>>(uri);
+      }
+      catch (Exception ex)
+      {
+        response = new AppResult<UsersListModel>
+        {
+          Errors = new List<string> { ex.Message },
+          Succeeded = false,
+          Response = new UsersListModel { Users = new List<UserModel>() }
+        };
+      }
 
-		private string GetRequestUri(string action)
-		{
-			string uri = this._apiIdentityUri + action;
-			return uri.ToLower();
-		}
-	}
+      return response!;
+    }
+
+    private string GetRequestUri(string action)
+    {
+      string uri = this._apiIdentityUri + action;
+      return uri.ToLower();
+    }
+  }
 }
