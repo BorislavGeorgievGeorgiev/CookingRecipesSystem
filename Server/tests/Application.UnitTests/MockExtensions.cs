4c4655a0f9a3365a08c4d67911844a8290815e99
Borislav <Borislav.G.Georgiev@gmail.com>
2022-10-11 16:44:46 +0300

    Server - IngredientCreateCommandTests - add 1 test.

---
+++ Server/tests/Application.UnitTests/MockExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+
+namespace CookingRecipesSystem.Application.UnitTests
+{
+	public static class MockExtensions
+	{
+		public static void SetupIQueryable<TRepository, TEntity>(
+			this Mock<TRepository> mock, IQueryable<TEntity> queryable)
+			where TRepository : class, IQueryable<TEntity>
+			where TEntity : class
+		{
+			mock.Setup(r => r.GetEnumerator()).Returns(() =>
+			{
+				var enumerator = queryable.GetEnumerator();
+				enumerator.Reset();
+				return enumerator;
+			});
+			mock.Setup(r => r.Provider).Returns(queryable.Provider);
+			mock.Setup(r => r.ElementType).Returns(queryable.ElementType);
+			mock.Setup(r => r.Expression).Returns(queryable.Expression);
+		}
+	}
+}
